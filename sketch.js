6569bc1da5ef160d3e7366f5055cc4c304eab5a5
Tanishka002 <siangsu2403@gmail.com>
2021-01-05 11:22:17 +0300

    first commit

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var constraintLog,chain;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -36,6 +37,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    constraintLog = new Log(200,180,80,PI/2);
+    
+    chain = new Chain(bird.body,constraintLog.body);  
 }
 
 function draw(){
@@ -61,4 +65,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constraintLog.display();
+    chain.display(); 
+    //line(bird.body.position.x,bird.body.position.y,constraintLog.body.position.x,constraintLog.body.position.y)
 }
